85113b9461e33f31906fa321283f6106a30a199d
Slenderick <er.galvez@duocuc.cl>
2023-10-15 22:23:28 -0300

    Metro de Santiago

---
+++ MetroSantiago/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'MetroSantiago',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
